39f92f80b3a1c3b24d0cbdfe6af6a5af96864c9d
Artem Kovalov <artemkov1990@gmail.com>
2022-09-13 10:45:01 +0300

    Initial commit

---
+++ src/ua/ithillel/exceptions/ArrayDataException.java
@@ -0,0 +1,8 @@
+package ua.ithillel.exceptions;
+
+public class ArraySizeException extends RuntimeException {
+
+  public ArraySizeException(String message) {
+    super(message);
+  }
+}
---
+++ src/ua/ithillel/exceptions/ArraySizeException.java
@@ -0,0 +1,8 @@
+package ua.ithillel;
+
+public class ArraySizeException extends RuntimeException {
+
+  public ArraySizeException(String message) {
+    super(message);
+  }
+}
